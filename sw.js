acbfdebb6b203ce88f369008b09248d12d00f5d7
Guilherme Benso <101438815+Guilherme-Benso@users.noreply.github.com>
2024-12-09 11:04:29 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,1 @@
-<!-- Builderall Global Script -->
-<script>
-(function(b, u, i, l, d, e, r) {
-    const config = {
-        bgskey: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1dWlkIjoiODdjOTM2OWItM2E2Mi00MGI2LWE2NTctNjg0YmQ1ZGYxYzA5In0.2joSxUk1ztdC8dcpsU8c7LOW1JNv7SSmnFNtkPBxVI8",
-        url: encodeURI(u.href)
-    }
-    d = b.createElement(i);
-    e = new URLSearchParams(config).toString()
-    d.src = ${l}/main.js?${e};
-    d.defer = !0;
-    b.head.appendChild(d);
-})(document, location, 'script', 'https://bgs.kub.app.builderall.io')
-</script>
+
